cccb02f4931412aeb7a7493d2bda8b6039e29405
Gui Dantas <guicha2002@gmail.com>
2023-04-26 12:12:48 +0100

    update

--- scripts/component.js
+++ scripts/component.js
@@ -1,147 +1,170 @@
 // Player (Bow)
-class Player{
-    constructor(x, y, w, h, ctx){
-        this.x = x; // 0
-        this.y = y;
-        this.w = w;
-        this.h = h;
-        this.ctx = ctx;
-        this.speedX = 0;
-        this.speedY = 0; 
-        
-        // Player Image
-        const bowImage = new Image();
-        bowImage.src="../images/bow.png";
-        this.img = bowImage;
-    }
+class Player {
+  constructor(x, y, w, h, ctx) {
+    this.x = x; // 0
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.ctx = ctx;
+    this.speedX = 0;
+    this.speedY = 0;
 
-    draw(){
-        this.ctx.drawImage(this.img, (this.x), (this.y), this.w, this.h);
-    }
+    // Player Image
+    const bowImage = new Image();
+    bowImage.src = "../images/bow.png";
+    this.img = bowImage;
+  }
 
-    newPos(){
-        this.x += this.speedX;
-        this.y += this.speedY;
-    }
+  draw() {
+    this.ctx.drawImage(this.img, this.x, this.y, this.w, this.h);
+  }
 
-    top(){
-        return this.y;
+  newPos() {
+    if (this.x <= 0) {
+      this.x = 0;
+    } else if (this.x + 90 >= 900) {
+      this.x = 810; // => 500-70
     }
+    this.x += this.speedX;
+  }
 
-    bottom(){
-        return this.y + this.h;
-    }
+  top() {
+    return this.y;
+  }
 
-    left(){
-        return this.x;
-    }
+  bottom() {
+    return this.y + this.h;
+  }
 
-    right(){
-        return this.x + this.w;
-    }
+  left() {
+    return this.x;
+  }
+
+  right() {
+    return this.x + this.w;
+  }
 }
 
 //Enemies
 class Enemy {
-    constructor(x, y, w, h, enemy, ctx) {
-      this.x = x;
-      this.y = y;
-      this.w = w;
-      this.h = h;
-      this.enemy = enemy; 
-      this.ctx = ctx;
-      this.speedX = 0;
-      this.speedY = 0;
-      this.arrows = [];
-      
-      //Enemie Image
-      const arrowImage = new Image();
-      arrowImage.src="../images/arrow.png";
-      this.img = arrowImage;
-    }
-  
-    draw() {
-      this.ctx.drawImage(this.img, this.x, this.y, this.w, this.h);
-    }
-  
-    newPos() {
-      if(this.enemy === true){
+  constructor(x, y, w, h, enemy, ctx, img) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.enemy = enemy;
+    this.ctx = ctx;
+    this.speedX = 0;
+    this.speedY = 0;
+    this.arrows = [];
+
+    //Enemie Image
+    const arrowImage = new Image();
+    arrowImage.src = img
+    this.img = arrowImage;
+  }
+
+  draw() {
+    this.ctx.drawImage(this.img, this.x, this.y, this.w, this.h);
+  }
+
+  newPos() {
+    if (this.enemy === true) {
       this.x = this.speedX;
       this.y = this.speedY;
-      } else {
-        this.x = this.x;
-        this.y = this.y;
-      }
+    } else {
+      this.x = this.x;
+      this.y = this.y;
     }
-
-    top() {
-      return this.y;
-    }
-  
-    bottom() {
-      return this.y + this.h;
-    }
-  
-    left() {
-      return this.x;
-    }
-  
-    right() {
-      return this.x + this.w;
-    }
-
-    crashWith(enemies){
-      return(this.bottom()>enemies.top() &&
-      this.top()<enemies.bottom() &&
-      this.right()>enemies.left() &&
-      this.left()<enemies.right())
-  }
   }
 
-  // Bird
-  class Bird{
-    constructor(x, y, w, h, ctx){
-        this.x = x; 
-        this.y = y;
-        this.w = w;
-        this.h = h;
-        this.ctx = ctx;
-        
-        // Bird Image
-        const birdImage = new Image();
-        birdImage.src="../images/bird.png";
-        this.img = birdImage;
+  top() {
+    return this.y;
+  }
+
+  bottom() {
+    return this.y + this.h;
+  }
+
+  left() {
+    return this.x;
+  }
+
+  right() {
+    return this.x + this.w;
+  }
+
+  crashWith(enemies) {
+    return (
+      this.bottom() > enemies.top() &&
+      this.top() < enemies.bottom() &&
+      this.right() > enemies.left() &&
+      this.left() < enemies.right()
+    );
+  }
+}
+
+// Bird
+class Bird {
+  constructor(x, y, w, h, ctx) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.ctx = ctx;
+    this.speedY = 1;
+
+    // Bird Image
+    const birdImage = new Image();
+    birdImage.src = "../images/bird.png";
+    this.img = birdImage;
+
+    this.maxTravel = false;
+  }
+
+  draw() {
+    this.ctx.drawImage(this.img, this.x, this.y, this.w, this.h);
+  }
+
+  newPos() {
+    if (this.y + this.h === 249) {
+      this.maxTravel = true;
+    }
+    if (this.y === 1) {
+      this.maxTravel = false;
     }
 
-    draw(){
-        this.ctx.drawImage(this.img, (this.x), (this.y), this.w, this.h);
+    if (this.y + this.h < canvas.height / 2 && !this.maxTravel) {
+      this.y += this.speedY;
     }
 
-    newPos(){
-        this.x = this.x;
-        this.y = this.y;
+    if (this.y > 0 && this.maxTravel) {
+      this.y -= this.speedY;
     }
+  }
 
-    top(){
-        return this.y;
-    }
+  top() {
+    return this.y;
+  }
 
-    bottom(){
-        return this.y + this.h;
-    }
+  bottom() {
+    return this.y + this.h;
+  }
 
-    left(){
-        return this.x;
-    }
+  left() {
+    return this.x;
+  }
 
-    right(){
-        return this.x + this.w;
-    }
+  right() {
+    return this.x + this.w;
+  }
 
-    crashWith(enemies){
-      return(this.bottom()>enemies.top() &&
-      this.top()<enemies.bottom() &&
-      this.right()>enemies.left() &&
-      this.left()<enemies.right())
+  crashWith(enemies) {
+    return (
+      this.bottom() > enemies.top() &&
+      this.top() < enemies.bottom() &&
+      this.right() > enemies.left() &&
+      this.left() < enemies.right()
+    );
   }
 }
--- scripts/game.js
+++ scripts/game.js
@@ -1,129 +1,133 @@
-class Game{
-    constructor(ctx, width, height, player, bird){
-        this.ctx = ctx;
-        this.width = width;
-        this.height = height;
-        this.player = player;
-        this.bird = bird;
-        this.intervalId = null;
-        this.frames = 0;
-        this.enemies = [];
-        this.arrows = [];
-        this.score = 0;
-        
-        
-        //Background Image
-        const desertImg = new Image();
-        desertImg.src="../images/jungle.jpg"
-        this.img = desertImg;
+class Game {
+  constructor(ctx, width, height, player, bird) {
+    this.ctx = ctx;
+    this.width = width;
+    this.height = height;
+    this.player = player;
+    this.bird = bird;
+    this.intervalId = null;
+    this.frames = 0;
+    this.enemies = [];
+    this.arrows = [];
+    this.score = 0;
 
+    //Background Image
+    const desertImg = new Image();
+    desertImg.src = "../images/jungle.jpg";
+    this.img = desertImg;
+  }
 
-    }
-    // Means Starting the Game
-    start(){
-        this.intervalId = setInterval(this.update, 10) //Updates the Game Each 10ms
-    }
-    // Updating the Game
-    update = () => {
-        this.frames++;
-        this.clear();
-        this.player.newPos();
-        this.player.draw();
-        this.bird.newPos()
-        this.bird.draw()
-        this.updateEnemies();
-        this.updateArrows();
-        this.checkBird();
-        this.checkGameOver();
+  // Means Starting the Game
+  start() {
+    this.intervalId = setInterval(this.update, 10); //Updates the Game Each 10ms
+  }
 
-        console.log(this.arrows.length)
-        
-    }
-    //Stops The Game
-    stop(){
-        clearInterval(this.intervalId);
-        startFlag = false;
-    }
-    //Clears Canvas with Background Image
-    clear(){
-        this.ctx.drawImage(this.img, 0, 0, this.width, this.height);
-    }
+  // Updating the Game
+  update = () => {
+    this.frames++;
+    this.clear();
+    this.player.newPos();
+    this.player.draw();
+    this.bird.newPos();
+    this.bird.draw();
+    this.updateEnemies();
+    this.updateArrows();
+    this.checkBird();
+    this.checkGameOver();
 
-    // Updates Enemies
-    updateEnemies(){
-    for (let i = 0; i<this.enemies.length ; i++){
-        this.enemies[i].x -= 1;// Enemy goes More to the Right
-        this.enemies[i].draw();// Continue to Draw Enemy
+    console.log(this.arrows.length);
+  };
+
+  //Stops The Game
+  stop() {
+    clearInterval(this.intervalId);
+    startFlag = false;
+  }
+
+  //Clears Canvas with Background Image
+  clear() {
+    this.ctx.drawImage(this.img, 0, 0, this.width, this.height);
+  }
+
+  // Updates Enemies
+  updateEnemies() {
+    for (let i = 0; i < this.enemies.length; i++) {
+      this.enemies[i].x -= 6; // Enemy goes More to the Right
+      this.enemies[i].draw(); // Continue to Draw Enemy
     }
-    if (this.frames % 200 === 0){
-        let x = 1200;
-        let minHeight = 10;// at least 10px of min Height
-        let maxHeight = 10;// max height of 10px
-            
-        let height = Math.floor(Math.random() * (maxHeight - minHeight + 1) + minHeight);
-        let randomY = Math.floor(Math.random() * 110);
-            
-    // Top Obstacles
-        this.enemies.push(new Enemy(x, randomY, 80, height, true, this.ctx));    
+    if (this.frames % 200 === 0) {
+      let x = 1200;
+      let minHeight = 10; // at least 10px of min Height
+      let maxHeight = 10; // max height of 10px
+
+      let height = Math.floor(
+        Math.random() * (maxHeight - minHeight + 1) + minHeight
+      );
+      let randomY = Math.floor(Math.random() * 110);
+
+      // Top Obstacles
+      this.enemies.push(
+        new Enemy(x, randomY, 80, height, true, this.ctx, "../images/enemyArrow.png")
+      );
+    }
+  }
+
+  // Shooting
+  shooting() {
+    let x = this.player.x + this.player.w / 2 - 5;
+    let y = this.player.y + this.player.h / 2 - 50;
+
+    this.arrows.push(
+      new Enemy(x, y, 10, 50, false, this.ctx, "../images/arrow.png")
+    );
+  }
+
+  // Updating the Player Arrows
+  updateArrows() {
+    for (let i = 0; i < this.arrows.length; i++) {
+      this.arrows[i].y -= 5;
+      this.arrows[i].newPos();
+      this.arrows[i].draw();
+
+      if (this.arrows[i].y < 0) {
+        this.arrows.splice(i, 1);
+      }
+
+      // Check for collision with Enemies
+      for (let j = 0; j < this.enemies.length; j++) {
+        if (this.arrows[i].crashWith(this.enemies[j])) {
+          this.arrows.splice(i, 1);
+          this.enemies.splice(j, 1);
+          this.score += 1
         }
+      }
     }
-    
-    // Shooting
-    shooting(){
- 
-                    let x = this.player.x + (this.player.w/2)-25;
-                    let y = this.player.y + (this.player.h / 2)-50;
-                    
-                    this.arrows.push(new Enemy(x, y, 50, 50, false, this.ctx));
+  }
 
+  // Check for collision between bird and enemies
+  checkBird() {
+    for (let j = 0; j < this.enemies.length; j++) {
+      if (this.bird.crashWith(this.enemies[j])) {
+        console.log("bird hit");
+        this.stop();
+      }
     }
-    
-    // Updating the Player Arrows
-    updateArrows(){
-        for (let i = 0; i < this.arrows.length; i++) {
-            this.arrows[i].y -= 3;
-            this.arrows[i].newPos();
-            this.arrows[i].draw();
+  }
 
-            if (this.arrows[i].y < 0){
-                this.arrows.splice(i, 1)
-            }
-    
-
-    // Check for collision with Enemies
-        for (let j = 0; j < this.enemies.length; j++) {
-            if (this.arrows[i].crashWith(this.enemies[j])) {
-                this.arrows.splice(i, 1);
-                this.enemies.splice(j, 1);
-                }
-            }
-        }
+  checkGameOver() {
+    const crashed = this.enemies.some((enemy) => {
+      return this.bird.crashWith(enemy);
+    });
+    if (crashed) {
+      ctx.fillStyle = "brown";
+      ctx.fillRect(250, 100, 400, 250);
+      ctx.font = "32px Arial";
+      ctx.fillStyle = "red";
+      ctx.fillText("Game Over", 365, 160);
+      ctx.fillStyle = "white";
+      ctx.fillText("Your final score:", 335, 230);
+      this.ctx.fillText(`${this.score}`, 433, 300);
+      this.stop();
     }
-
-    checkBird(){
-            // Check for collision between bird and enemies
-            for (let j = 0; j < this.enemies.length; j++) {    
-                if (this.bird.crashWith(this.enemies[j])) {
-                   console.log("bird hit")
-                   this.stop()
-                    }
-            }
-    }
-
-    checkGameOver(){
-        const crashed = this.enemies.some((enemy)=>{
-            return this.bird.crashWith(enemy);
-        })
-        if(crashed){
-            ctx.fillStyle = 'brown';
-            ctx.fillRect(50, 200, 400, 250);
-            ctx.font = '32px Helvetica';
-            ctx.fillStyle = 'red';
-            ctx.fillText('Game Over', 150, 300);
-            ctx.fillStyle = 'white';
-            ctx.fillText('Your final score', 135, 350);
-            this.ctx.fillText(`${this.score}`, 230, 400);
-            this.stop();
-        }
-    }
+  }
 }
